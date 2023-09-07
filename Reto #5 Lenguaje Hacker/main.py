b33f50980118a88d7281ef88d70879a7126e350a
Baaaalser <43592336+Baaaalser@users.noreply.github.com>
2023-09-07 17:49:32 -0300

    ejercicio resuelto!

--- Reto #5 Lenguaje Hacker/main.py
+++ Reto #5 Lenguaje Hacker/main.py
@@ -8,5 +8,23 @@
 #  *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
 #  */
 
-def convert_to_hacker_language():
-    pass
+def convert_to_hacker_language(text_to_covert : str):
+
+    dictionary = {"a":"4","b":"I3","c":"[","d":")",
+                    "e":"3","f":"|=","g":"&","h":"#",
+                    "i":"1","j":",_|","k":">|","l":"1",
+                    "m":"/\/\\","n":"^/","o":"0","p":"|*",
+                    "q":"(_,)","r":"I2","s":"5","t":"7",
+                    "u":"(_)","v":"\/","w":"\/\/","x":"><",
+                    "y":"j","z":"2"," ":" "}
+    
+    #first i need to lowercase te word so i can check against the dict
+    text_to_covert = text_to_covert.lower()
+    #now is ready for conversion
+    #printing one by one for comparisson
+    for letter in text_to_covert:
+        print(dictionary[letter],letter)
+    #the join version
+    return "".join(dictionary[letter] for letter in text_to_covert)    
+mytext = 'Hola Mundo'
+print(convert_to_hacker_language(mytext))
