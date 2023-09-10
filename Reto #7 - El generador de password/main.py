32b8596e8f3ff43b5b2fabff1d7db05cd3e1ceef
Baaaalser <43592336+Baaaalser@users.noreply.github.com>
2023-09-10 12:28:30 -0300

    solved exercise

--- Reto #7 - El generador de password/main.py
+++ Reto #7 - El generador de password/main.py
@@ -7,3 +7,46 @@
 #  * - Con o sin símbolos.
 #  * (Pudiendo combinar todos estos parámetros entre ellos)
 #  */
+
+
+import random
+
+letters = 'abcdefghijklmnopqrstuvwxyz'
+nums = '0123456789'
+symbs = '~!@#$%^&*()+?><|'
+
+password = ''
+
+password += letters
+
+print('Generador de contraseñas')
+while True:
+    length = int(input('Ingrese longitud de la contraseña(8-16): '))
+    if (length >= 8 or length <=16):
+        break
+
+while True:
+    uppercase = input('Usar mayúsculas? S/N:')
+    if(uppercase.lower() == 's'):
+        password += letters.upper()
+        break
+    if(uppercase.lower() == 'n'):
+        break
+
+while True:
+    numbers = input('Usar números? S/N:')
+    if(numbers.lower() == 's'):
+        password += nums
+        break
+    if(numbers.lower() == 'n'):
+        break
+
+while True:
+    symbols = input('Usar símbolos? S/N:')
+    if(symbols.lower() == 's'):
+        password += symbs
+        break
+    if(symbols.lower() == 'n'):
+        break
+
+print(f'Password = {"".join(random.sample(password,length))}')
