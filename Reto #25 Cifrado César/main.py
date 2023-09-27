f40a550c83d39028214bb052a2f6d8da66944853
Baaaalser <43592336+Baaaalser@users.noreply.github.com>
2023-09-27 13:31:12 -0300

    eliminado cod repetido

--- Reto #25 Cifrado César/main.py
+++ Reto #25 Cifrado César/main.py
@@ -11,7 +11,7 @@ import re
 def rotate_list(l, n):
     return l[n:] + l[:n]
 
-def Caesars_cipher(text:str,rot : int):
+def Caesars_cipher(text:str,rot : int,decipher: bool):
     abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     rotated_abc = rotate_list(abc,rot)
     translated = ''
@@ -19,27 +19,19 @@ def Caesars_cipher(text:str,rot : int):
     for word in text:
         filtered = re.search(r"[a-zA-Z]", word, re.MULTILINE)
         if filtered is not None:
-            translated += rotated_abc[abc.index(filtered[0].lower())]
+            if not decipher:
+                translated += rotated_abc[abc.index(filtered[0].lower())]
+            else:
+                translated += abc[rotated_abc.index(filtered[0].lower())]
         else:
             translated += word
     print(translated)
 
-def Caesars_decipher(text:str,rot : int):
-    abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    rotated_abc = rotate_list(abc,rot)
-    translated = ''
 
-    for word in text:
-        filtered = re.search(r"[a-zA-Z]", word, re.MULTILINE)
-        if filtered is not None:
-            translated += abc[rotated_abc.index(filtered[0].lower())]
-        else:
-            translated += word
-    print(translated)
-Caesars_cipher('Ser o no ser, esa es la cuestión.',3)
-Caesars_decipher('vhu r qr vhu, hvd hv od fxhvwlóq.',3)
+Caesars_cipher('Ser o no ser, esa es la cuestión.',3,False)
+Caesars_cipher('vhu r qr vhu, hvd hv od fxhvwlóq.',3,True)
 
-Caesars_cipher('Ser o no ser, esa es la cuestión.',14)
-Caesars_decipher('gsf c bc gsf, sgo sg zo qisghwób.',14)
+Caesars_cipher('Ser o no ser, esa es la cuestión.',14,False)
+Caesars_cipher('gsf c bc gsf, sgo sg zo qisghwób.',14,True)
 
 
